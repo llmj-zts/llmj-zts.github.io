489731f4b0245de2544cc1c780516c44ea56ee49
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-02 00:29:16 +0800

    Update getapi.js

--- js/getapi.js
+++ js/getapi.js
@@ -1,7 +1,3 @@
-var httpRequest = new XMLHttpRequest();
-httpRequest.open("GET","https://www.baidu.com",false);
-//httpRequest.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0");
-httpRequest.send();
-httpRequest.onreadystatechange = (e) => {
-  console.log(Http.responseText);
-}
+$.get('https://www.baidu.com', function(data) {
+    console.log(data); // 处理获取到的数据
+});
