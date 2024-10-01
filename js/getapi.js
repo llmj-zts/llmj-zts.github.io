253db3324895d897c90a8b5c88a453137d302190
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-01 12:09:38 +0800

    Update getapi.js

--- js/getapi.js
+++ js/getapi.js
@@ -1,5 +1,6 @@
 var httpRequest = new XMLHttpRequest();
 httpRequest.open("GET","https://www.baidu.com",true);
+response.setHeader("Access-Control-Allow-Origin", "*");
 httpRequest.onload=function(){
 	console.log(httpRequest.status);
 }
