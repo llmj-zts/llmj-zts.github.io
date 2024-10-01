495fdadaee1e54edc2c031aaf21891514b5c7121
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-02 01:07:30 +0800

    Update getapi.js

--- js/getapi.js
+++ js/getapi.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-script.src = 'https://api.example.com/data?callback=handleResponse'; // 替换为实际的 API URL
+script.src = 'https://api.baidu.com'; // 替换为实际的 API URL
 document.body.appendChild(script);
