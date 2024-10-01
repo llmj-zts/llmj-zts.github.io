c5e2b28a42360c9163c6355c51762a6f352b6fae
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-02 00:39:45 +0800

    Update getapi.js

--- js/getapi.js
+++ js/getapi.js
@@ -3,5 +3,6 @@ httpRequest.open("GET","https://www.baidu.com");
 //httpRequest.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0");
 httpRequest.send();
 httpRequest.onreadystatechange = () => {
-  console.log(Http.status);
+  console.log(httpRequest.status);
+  console.log(httpRequest);
 }
