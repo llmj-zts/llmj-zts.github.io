f39b753d2074be1846cd82686afcd4b8a2b8c173
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-01 11:51:10 +0800

    Add files via upload

---
+++ js/getapi.js
@@ -0,0 +1,6 @@
+var httpRequest = new XMLHttpRequest();
+httpRequest.open("GET","https://www.baidu.com",true);
+httpRequest.onload=function(){
+	console.log(httpRequest.status);
+}
+httpRequest.send();
