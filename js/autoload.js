fcfbcb751b878ad2c3f0e2d01a82aac84d9ce8e9
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-04 15:37:33 +0800

    Update autoload.js

--- js/autoload.js
+++ js/autoload.js
@@ -1,18 +1,18 @@
 // 加载 CSS
-$("<link>").attr({href: "../assert/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+$("<link>").attr({href: "../assets/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
 
 // 插入 DIV
 $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
 
 // 加载 JS
 $.ajax({
-	url: '../assert/waifu-tips.js',
+	url: '../assets/waifu-tips.js',
 	dataType:"script",
 	cache: true,
 	async: false
 });
 $.ajax({
-	url: '../assert/live2d.js',
+	url: '../assets/live2d.js',
 	dataType:"script",
 	cache: true,
 	async: false
@@ -24,4 +24,4 @@ $.ajax({
 live2d_settings['modelId'] = 5;                  // 默认模型 ID
 live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
 /* 在 initModel 前添加 */
-initModel('../assert/waifu-tips.json');
+initModel('../assets/waifu-tips.json');
