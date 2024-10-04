1245e856aba3b97f14942598b59a1888c7cb4539
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-04 22:36:33 +0800

    Update autoload.js

--- js/autoload.js
+++ js/autoload.js
@@ -1,13 +1,19 @@
 try {
-    $("<link>").attr({href: "../assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "../assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
-    $.ajax({url: '../assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
-    $.ajax({url: '../assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
+    $.ajax({url: '../assets/waifu-tips.min.js',dataType:"script", cache: true, async: false});
+    $.ajax({url: '../assets/live2d.min.js',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
-    live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
-    live2d_settings['modelId'] = 5;                  // 默认模型 ID
-    live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
-    live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+    live2d_settings['modelId'] = 2;                  // 默认模型 ID
+    live2d_settings['modelTexturesId'] = 6;          // 默认材质 ID
+	live2d_settings['showHitokoto']         = true;         // 显示一言
+	live2d_settings['showF12Status']        = true;         // 显示加载状态
+	live2d_settings['showF12OpenMsg']       = true;         // 显示控制台打开提示
+	live2d_settings['showCopyMessage']      = true;         // 显示 复制内容 提示
+    live2d_settings['homePageUrl']          = 'index.html';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
+    live2d_settings['aboutPageUrl']         = 'https://www.fghrsh.net/post/123.html';   // 关于页地址, '{URL 网址}'
+    live2d_settings['screenshotCaptureName']= 'llmj.png'; // 看板娘截图文件名，例如 'live2d.png'
     /* 在 initModel 前添加 */
     initModel('../assets/waifu-tips.json');
 } catch(err) { console.log('[Error] JQuery is not defined.') }
+
