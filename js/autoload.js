47372a8d800981a9603d41c1fec176a76f4c89d5
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-04 15:54:40 +0800

    Update autoload.js

--- js/autoload.js
+++ js/autoload.js
@@ -1,5 +1,5 @@
 // 加载 CSS
-$("<link>").attr({href: "../assets/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+$("<link>").attr({href: "../assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
 
 // 插入 DIV
 $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
