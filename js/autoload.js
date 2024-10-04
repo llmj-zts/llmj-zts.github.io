24ff593ba583e0d47cf94e2fad215c188a424067
llmj <129515918+shuai54088666@users.noreply.github.com>
2024-10-04 22:11:40 +0800

    Update autoload.js

--- js/autoload.js
+++ js/autoload.js
@@ -6,11 +6,14 @@ try {
     /* 可直接修改部分参数 */
     live2d_settings['modelId'] = 2;                  // 默认模型 ID
     live2d_settings['modelTexturesId'] = 6;          // 默认材质 ID
-	live2d_settings['modelStorage']         = false;         // 记录 ID (刷新后恢复)，可选 true(真), false(假)
-	live2d_settings['showHitokoto']         = true;         // 显示一言
-	live2d_settings['showF12Status']        = true;         // 显示加载状态
-	live2d_settings['showF12OpenMsg']       = true;         // 显示控制台打开提示
-	live2d_settings['showCopyMessage']      = true;         // 显示 复制内容 提示
+    live2d_settings['modelStorage']         = false;         // 记录 ID (刷新后恢复)，可选 true(真), false(假)
+    live2d_settings['showHitokoto']         = true;         // 显示一言
+    live2d_settings['showF12Status']        = true;         // 显示加载状态
+    live2d_settings['showF12Message']       = false;        // 显示看板娘消息
+    live2d_settings['showF12OpenMsg']       = true;         // 显示控制台打开提示
+    live2d_settings['showCopyMessage']      = true;         // 显示 复制内容 提示
+    live2d_settings['showWelcomeMessage']   = true;         // 显示进入面页欢迎词
+    live2d_settings['waifuDraggable']       = 'axis-x';
     live2d_settings['homePageUrl']          = 'index.html';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
     live2d_settings['aboutPageUrl']         = 'https://www.fghrsh.net/post/123.html';   // 关于页地址, '{URL 网址}'
     live2d_settings['screenshotCaptureName']= 'llmj.png'; // 看板娘截图文件名，例如 'live2d.png'
