fc8c7a8cb88e36a7f8b54cbe47f6f36d77793390
llmj-zts <qaz35870661@163.com>
2025-10-18 17:34:45 +0800

    Update loadrand-phone.js

--- js/loadrand-phone.js
+++ js/loadrand-phone.js
@@ -6,7 +6,15 @@ $.getJSON("https://zts.llmj.dpdns.org/picture/img-phone.json",function(data){
 	ment.style.backgroundImage='url('+backgroundimg[rand].url+')';
 });
 const randmusic=['https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/07273409337d9fa9/j.mp3','https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/e64416e0f13fafba/n.mp3','https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/faad2da895e72f61/t.mp3','https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/c1ae83a0262c6a74/m.mp3','https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/6ce8581ce79ef4ee/lq.mp3']
-const randimg=['https://youke1.picui.cn/s1/2025/08/10/68983b576d539.png','https://youke1.picui.cn/s1/2025/08/10/68983b600f6f9.png','https://youke1.picui.cn/s1/2025/08/10/68983b58887bf.png','https://youke1.picui.cn/s1/2025/08/10/68983b57c1502.png','https://youke1.picui.cn/s1/2025/08/10/68983b5ec70f9.png']
+const randimg=['../picture/kunb-phone.png','../picture/kun-phone.png','../picture/memes-phone.png','../picture/kunkun.png','../picture/lq-phone.png']
+const ikun=document.getElementById("ikun");
+var isMouseDown=false;
+var jumpkunnumber=0;
+var listimg=[];//预加载
+for(i=0;i<5;i++){
+	var img=new Image();
+	img.src=randimg[i];
+	listimg[i]=img;}
 function download(name){
 	$.getJSON("https://zts.llmj.dpdns.org/html-phone/do.json",function(data){
 		var size=data[name]["size"];
@@ -17,5 +25,62 @@ function download(name){
 		window.open(url,"_self");
 	})
 }
+function kun(){
+	isMouseDown=true;
+	jumpkunnumber+=1;
+	var randomnumber=Math.floor(Math.random()*randmusic.length);
+	var music=new Audio(randmusic[randomnumber]);
+	var angle=0;
+	var opacity=0;
+	var size=0;
+	var sizeadd=5;
+	const otherjumpkun=jumpkunnumber;
+	const imgplay=listimg[randomnumber]
+	music.play();
+	ikun.width=window.innerWidth;
+	ikun.height=window.innerHeight;
+	var mykun=ikun.getContext("2d");
+	mykun.translate(event.clientX,event.clientY);
+	function jumpkun(){
+		mykun.clearRect(-100,-100,ikun.width,ikun.height);
+		ikun.style.opacity=opacity;
+		mykun.rotate(angle);
+		mykun.drawImage(imgplay,-50,-50,size,size);
+		angle+=0.11;
+		opacity+=0.02;
+		//console.log(angle);
+		if(!isMouseDown){mykun.clearRect(-100,-100,ikun.width,ikun.height);return;}
+		if(jumpkunnumber>otherjumpkun){
+			mykun.clearRect(-100,-100,ikun.width,ikun.height);
+			return;}
+		if(size==100){
+			music=new Audio("../music/xnj.mp3");
+			music.play();
+			drawkun();}
+		else{
+			size+=10;
+			requestAnimationFrame(jumpkun);
+		}
+	}
+	function drawkun(){
+		mykun.clearRect(-100,-100,ikun.width,ikun.height);
+		mykun.drawImage(imgplay,-50,-50,size,size);
+		ikun.style.opacity=opacity;
+		if(isMouseDown){
+			size+=sizeadd;
+			opacity+=0.02;
+			if(size>150){sizeadd=-5;}
+			if(size<100){sizeadd=5;}
+			requestAnimationFrame(drawkun);
+		}else{
+			music.pause();
+			mykun.clearRect(-100,-100,ikun.width,ikun.height);
+			return;}
+	}jumpkun();
+}
 
-
+//这里是kun的点击
+document.addEventListener('mousedown',kun);
+document.addEventListener('mouseup',function(event){
+	isMouseDown=false;
+});
